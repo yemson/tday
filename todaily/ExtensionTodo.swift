2a804b7b07feaf5b92241a538b678c59fa31e20e
yemson <lier@kakao.com>
2022-01-20 17:58:49 +0900

    feat: 날씨 정보 확인 추가

--- todaily/ExtensionTodo.swift
+++ todaily/ExtensionTodo.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 extension Formatter {
     static let weekDay: DateFormatter = {
@@ -14,4 +15,20 @@ extension Formatter {
         formatter.dateFormat = "dd일, EEEE"
         return formatter
     }()
+}
+
+extension UIApplication {
+    func hideKeyboard() {
+        guard let window = windows.first else { return }
+        let tapRecognizer = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+        tapRecognizer.cancelsTouchesInView = false
+        tapRecognizer.delegate = self
+        window.addGestureRecognizer(tapRecognizer)
+    }
+ }
+
+extension UIApplication: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return false
+    }
 }
--- todaily/TodayTodoView.swift
+++ todaily/TodayTodoView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 import XCTest
 
+struct Weather: Decodable {
+    var main: String
+    var description: String
+}
+
+struct WeatherResponse: Decodable {
+    let weather: [Weather]
+}
+
 struct TodayTodoView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
     @State private var todoContent: String = ""
     @State private var showAddTodoModal: Bool = false
     @State private var showingAlert: Bool = false
+    @State private var showWeather: String = ""
     
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Todo.content, ascending: true)], animation: .default)
     private var todos: FetchedResults<Todo>
@@ -24,7 +34,7 @@ struct TodayTodoView: View {
                 .edgesIgnoringSafeArea(.all)
             VStack {
                 HStack {
-                    Button(action: {self.showAddTodoModal = true}) {
+                    Button(action: {showAddTodoModal = true}) {
                         Label("", systemImage: "line.3.horizontal")
                             .font(.system(size: 25))
                             .foregroundColor(Color("TodoRed"))
@@ -75,20 +85,29 @@ struct TodayTodoView: View {
                     }
                     .listRowBackground(Color("TodoBeige"))
                     .listRowSeparator(.hidden)
-                    
                 }
                 .environment(\.locale, Locale(identifier: "ko"))
                 .listStyle(.plain)
                 Spacer()
+                HStack {
+                    Spacer()
+                    Label("", systemImage: showWeather)
+                        .font(.system(size: 50))
+                        .foregroundColor(Color("TodoBlue"))
+                        .padding()
+                    Spacer()
+                }
             }
             .sheet(isPresented: $showAddTodoModal) {
                 AddTodoModalView()
             }
             .alert(isPresented: $showingAlert) {
-                Alert(title: Text("빈 칸을 등록할 수 없습니다!"), message: nil,
+                Alert(title: Text("빈 칸은 등록할 수 없습니다!"), message: nil,
                       dismissButton: .default(Text("확인")))
-            }
+            }.onAppear(perform: loadData)
+                .onAppear (perform : UIApplication.shared.hideKeyboard)
         }
+        .ignoresSafeArea(.keyboard)
     }
     
     private func addTodo() {
@@ -112,7 +131,67 @@ struct TodayTodoView: View {
         PersistenceController.shared.saveContext()
         print(todo.state)
     }
+    
+    func loadData() {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=Busan&appid=8e71ae892d923505cdcff82dafb4ed43") else {
+            fatalError("Invalid URL")
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            let result = try? JSONDecoder().decode(WeatherResponse.self, from: data)
+            if let result = result {
+                print(result)
+                result.weather.forEach {
+                    print($0.main)
+                    print($0.description)
+                    switch $0.description {
+                    case "clear sky":
+                        showWeather = "sun.max"
+                        break
+                    case "few clouds":
+                        showWeather = "cloud.sun"
+                        break
+                    case "scattered clouds":
+                        showWeather = "cloud"
+                        break
+                    case "broken clouds":
+                        showWeather = "cloud"
+                        break
+                    case "shower rain":
+                        showWeather = "cloud.drizzle"
+                        break
+                    case "rain":
+                        showWeather = "cloud.rain"
+                        break
+                    case "thunderstorm":
+                        showWeather = "cloud.bolt"
+                        break
+                    case "snow":
+                        showWeather = "cloud.snow"
+                        break
+                    case "mist":
+                        showWeather = "cloud.fog"
+                        break
+                    default:
+                        break
+                    }
+                }
+            }
+        }.resume()
+    }
 }
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
 
 struct TodayTodoView_Previews: PreviewProvider {
     static var previews: some View {
